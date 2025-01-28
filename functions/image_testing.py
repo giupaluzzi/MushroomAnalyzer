6808c699e5a284790c06719b66d868a9b55ad79d
Stefano Morici <morici.2021045@studenti.uniroma1.it>
2025-01-28 21:40:53 +0100

    Modifica commenti

--- functions/image_testing.py
+++ functions/image_testing.py
@@ -1,49 +1,50 @@
 import json
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+from keras.utils import load_img, img_to_array
 import matplotlib.pyplot as plt
 
-# Funzione per testare un'immagine singola
 def image_testing(model_path, image_path, image_size):
-    # Carica il modello salvato
+    # Tests an image
+    
+    # Load the saved model
     model = load_model(model_path)
 
     # Ricompila il modello per evitare l'avviso
     # model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
 
-    # Carica il dizionario delle classi
+    # Load the dictionary with the classes
     with open('class_indices.json', 'r') as f:
         class_indices = json.load(f)
 
-    # Inverti il dizionario per ottenere un mapping indice -> nome classe
+    # Invert the dictionary to obtain a mapping of index -> class name
     index_to_class = {v: k for k, v in class_indices.items()}
 
-    # Preprocessa l'immagine
+    # Preprocess the image
     img = load_img(image_path, target_size=image_size)
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0  # Normalizzazione
+    img_array = img_array / 255.0  # Normalize
 
-    # Predici la classe
+    # Predict the class
     predictions = model.predict(img_array)
     predicted_class_idx = np.argmax(predictions)
     predicted_class_name = index_to_class[predicted_class_idx]
 
-    # Visualizza l'immagine con la classe predetta
+    # Display the image with the predicted class
     plt.figure(figsize=(6, 6))
     plt.imshow(load_img(image_path))
-    plt.title(f"Classe Predetta: {predicted_class_name}", fontsize=14)
+    plt.title(f"Predicted Class: {predicted_class_name}", fontsize=14)
     plt.axis('off')
     plt.show()
 
     return predicted_class_name
 
-# Percorsi
-model_path = "Mushrooms_model_3.h5"  # Modifica con il percorso corretto del tuo modello
+# Paths
+model_path = "Mushrooms_model_3.h5"  
 image_path = "MushroomAnalyzer/test/marasmius_griseoradiatus.jpg"
 image_size = (224, 224)
 
-# Test e visualizzazione dell'immagine
+# Test and display of the image
 predicted_class = image_testing(model_path, image_path, image_size)
-print(f"Classe predetta: {predicted_class}")
+print(f"Predicted Class: {predicted_class}")
